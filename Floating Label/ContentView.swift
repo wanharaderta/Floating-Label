56e90c1b22930c6b08b37651562e7d0a646b4137
wanhar <wanhar@sehatq.com>
2020-08-16 22:50:10 +0700

    master

--- Floating Label/ContentView.swift
+++ Floating Label/ContentView.swift
@@ -8,13 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var firstName: String
+    @State var lastName: String
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        return VStack(alignment:.center){
+            ZStack(alignment:.leading) {
+                Text("First Name")
+                    .foregroundColor($firstName.wrappedValue.isEmpty ? Color(.placeholderText) : .accentColor)
+                    .offset(y: $firstName.wrappedValue.isEmpty ? 0 : -25)
+                    .scaleEffect($firstName.wrappedValue.isEmpty ? 1 : 0.75, anchor: .leading)
+                TextField("",text: $firstName)
+            }.padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/, 12)
+            ZStack(alignment:.leading) {
+                Text("Last Name")
+                    .foregroundColor($lastName.wrappedValue.isEmpty ? Color(.placeholderText) : .accentColor)
+                    .offset(y: $lastName.wrappedValue.isEmpty ? 0 : -25)
+                    .scaleEffect($lastName.wrappedValue.isEmpty ? 1 : 0.75, anchor: .leading)
+                TextField("",text: $lastName)
+            }.padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/, 12)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(firstName: "", lastName: "")
     }
 }
--- Floating Label/Floating_LabelApp.swift
+++ Floating Label/Floating_LabelApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Floating_LabelApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(firstName: "", lastName: "")
         }
     }
 }
