f76afec86bb52571d3a8cb320b37ca3181f5c6aa
wanhar <wanhar@sehatq.com>
2020-08-16 22:35:22 +0700

    Initial Commit

---
+++ Floating Label/Floating_LabelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Floating_LabelApp.swift
+//  Floating Label
+//
+//  Created by Wanhar on 16/08/20.
+//
+
+import SwiftUI
+
+@main
+struct Floating_LabelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
