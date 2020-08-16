42834e948edca432e2540829188cff430ccb73fc
wanhar <wanhar@sehatq.com>
2020-08-16 23:26:09 +0700

    title

--- Floating Label/ContentView.swift
+++ Floating Label/ContentView.swift
@@ -16,14 +16,14 @@ struct ContentView: View {
         return VStack(alignment:.center){
             Text("Floating Labels")
                 .font(.largeTitle)
+                .padding(.bottom, 20)
             
             ZStack(alignment:.leading) {
                 Text("First Name")
                     .foregroundColor($firstName.wrappedValue.isEmpty ? Color(.placeholderText) : .accentColor)
-                    .offset(y: $firstName.wrappedValue.isEmpty ? 0 : -30)
+                    .offset(y: $firstName.wrappedValue.isEmpty ? 0 : -35)
                     .scaleEffect($firstName.wrappedValue.isEmpty ? 1 : 0.7, anchor: .leading)
                 TextField("",text: $firstName)
-                    .padding(.top, 12)
             }.padding([.leading,.trailing], 12)
             .animation(.spring(response: 0.3, dampingFraction: 0.5, blendDuration: 0.2))
             
@@ -33,12 +33,11 @@ struct ContentView: View {
             ZStack(alignment:.leading) {
                 Text("Last Name")
                     .foregroundColor($lastName.wrappedValue.isEmpty ? Color(.placeholderText) : .accentColor)
-                    .offset(y: $lastName.wrappedValue.isEmpty ? 0 : -30)
+                    .offset(y: $lastName.wrappedValue.isEmpty ? 0 : -35)
                     .scaleEffect($lastName.wrappedValue.isEmpty ? 1 : 0.7, anchor: .leading)
                 TextField("",text: $lastName)
-                    .padding(.top, 12)
             }.padding([.leading,.trailing], 12)
-            .padding(.top, 20)
+            .padding(.top, 30)
             .animation(.spring(response: 0.3, dampingFraction: 0.5, blendDuration: 0.2))
             
             Divider()
